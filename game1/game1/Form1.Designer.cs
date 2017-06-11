45ed152408e47ae349e914f14a6746d3834aaf82
martynab95 <martynaborzyszkowska@wp.pl>
2017-06-11 20:41:27 +0200

    Tło

---
+++ game1/game1/Form1.Designer.cs
@@ -0,0 +1,48 @@
+﻿namespace game1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Wymagana zmienna projektanta.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Wyczyść wszystkie używane zasoby.
+        /// </summary>
+        /// <param name="disposing">prawda, jeżeli zarządzane zasoby powinny zostać zlikwidowane; Fałsz w przeciwnym wypadku.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Kod generowany przez Projektanta formularzy systemu Windows
+
+        /// <summary>
+        /// Wymagana metoda obsługi projektanta — nie należy modyfikować 
+        /// zawartość tej metody z edytorem kodu.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.SuspendLayout();
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = global::game1.Properties.Resources.tlo;
+            this.ClientSize = new System.Drawing.Size(454, 364);
+            this.Name = "Form1";
+            this.Text = "Game1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+    }
+}
+
